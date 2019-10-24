3e1ed400318080e2f4ad6a3de3c20fcfdfe4ca02
Tim Sewell <tim.sewell@uk.aswatson.com>
2019-10-24 13:24:00 +0100

    Changed favicon

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={santa} className="App-logo" alt="logo" />
         <p>
-          The Secret Santa machine
+          The Secret Santa Machine
         </p>
       </header>
     </div>
---
+++ src/config.js
@@ -0,0 +1,4 @@
+const config = {
+    api: 'https://api.myjson.com/bins/72cvw'
+};
+export default config;
