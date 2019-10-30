86c243156471b9c0c03c6c85ed23df18323aa2c9
Tim Sewell <sewell.tim@gmail.com>
2019-10-30 21:26:36 +0000

    Tidying up santaMailer function

--- functions/index.js
+++ functions/index.js
@@ -5,51 +5,66 @@ const nodeMailer = require('nodemailer');
 exports.santaMailer = functions.region('europe-west2')
     .firestore.document('mail/{mailId}')
     .onCreate(async (snapshot, context) => {
-        const params = snapshot.data();
+        return new Promise(async (resolve, reject) => {
+            const params = snapshot.data();
 
-        const transporter = nodeMailer.createTransport({
-            host: 'smtp.gmail.com',
-            port: 465,
-            secure: true,
-            auth: {
-                user: 'thesecretsantamachine@gmail.com',
-                pass: 'sxpegwroxxxkvxcy'
-            }
-        });
-
-        const sendConfirmation = (aOptions) => {
-            let mailOptions = {
-                to: 'sewell.tim@gmail.com',
-                subject: 'Secret Santa message sent',
-                html: 'Sent to ' + params.name + '(' + aOptions.to + ') at ' + new Date()
-            };
-            send(mailOptions);
-        };
-
-        const send = (mailOptions) => {
-            return transporter.sendMail(mailOptions, (error, info) => {
-                if (error) {
-                    console.log(error);
-                    snapshot.ref.update({
-                        sent: false,
-                        sendTime: new Date(),
-                        error: error
-                    });
+            const transporter = nodeMailer.createTransport({
+                host: 'smtp.gmail.com',
+                port: 465,
+                secure: true,
+                auth: {
+                    user: 'thesecretsantamachine@gmail.com',
+                    pass: 'sxpegwroxxxkvxcy'
                 }
-                console.log('Message %s sent: %s', info.messageId, info.response);
-                snapshot.ref.update({
-                    sent: true,
-                    sendTime: new Date()
-                });
             });
-        };
 
-        let mailOptions = {
-            to: params.to,
-            subject: params.message.subject,
-            html: params.message.html,
-            text: params.message.text
-        };
-        await send(mailOptions);
-        sendConfirmation(mailOptions);
+            const sendConfirmation = (aOptions) => {
+                let mailOptions = {
+                    to: 'sewell.tim@gmail.com',
+                    subject: 'Secret Santa message sent',
+                    html: 'Sent to ' + params.name +
+                        '(' + aOptions.to + ') at ' + new Date()
+                };
+                send(mailOptions, true);
+            };
+
+            const send = (mailOptions, isLast) => {
+                return transporter.sendMail(mailOptions, (error, info) => {
+                    if (error) {
+                        console.log(error);
+                        if (!isLast) {
+                            snapshot.ref.update({
+                                sent: false,
+                                sendTime: new Date(),
+                                error: error
+                            });
+                        }
+                        else {
+                            reject(error);
+                        }
+                    }
+                    console.log('Message %s sent: %s',
+                        info.messageId,
+                        info.response);
+                    if (!isLast) {
+                        snapshot.ref.update({
+                            sent: true,
+                            sendTime: new Date()
+                        });
+                    }
+                    else {
+                        resolve();
+                    }
+                });
+            };
+
+            let mailOptions = {
+                to: params.to,
+                subject: params.message.subject,
+                html: params.message.html,
+                text: params.message.text
+            };
+            await send(mailOptions);
+            sendConfirmation(mailOptions);
+        });
 });
